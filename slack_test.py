ae968b477f7e47ba51bc7c22888785712a87c77d
ilovemycat666 <53376392+ilovemycat666@users.noreply.github.com>
2021-06-02 16:18:18 -0400

    Update slack_test.py

--- slack_test.py
+++ slack_test.py
@@ -2,7 +2,7 @@
 
 
 from slack_sdk.webhook import WebhookClient
-url = "https://hooks.slack.com/services/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"
+url = "https://hooks.slack.com/services/your specific address here"
 webhook = WebhookClient(url)
 
 message = "Another"
@@ -12,18 +12,3 @@ assert response.status_code == 200
 assert response.body == "ok"
 
 
-
-# richer text
-# webhook = WebhookClient(url)
-# response = webhook.send(
-#     text="fallback",
-#     blocks=[
-#         {
-#             "type": "section",
-#             "text": {
-#                 "type": "mrkdwn",
-#                 "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
-#             }
-#         }
-#     ]
-# )
